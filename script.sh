9216cf53ba7d1e3840f048c43d283a3dfc3a01e1
NAThompson <nathompson7@protonmail.com>
2016-01-17 22:15:22 -0600

    Add info on how to use autoscaler.

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get install -y nginx emacs
 sudo chmod a+rw /var/www/html/index.nginx-debian.html
 sudo echo "Our startup script works" > /var/www/html/index.nginx-debian.html
