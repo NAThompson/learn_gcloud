b45612392ef2cb25a0d65b259399df0a93d66af2
NAThompson <nathompson7@protonmail.com>
2016-01-18 23:42:53 -0600

    Clean up

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install -y nginx emacs
 sudo chmod a+rw /var/www/html/index.nginx-debian.html
-sudo echo "Our startup script works" > /var/www/html/index.nginx-debian.html
+sudo echo "Hello from $(hostname)" > /var/www/html/index.nginx-debian.html
